7398c3f7fbd08dc1e4caa3adedbe0fdad4c9b365
NHANGUYEN <nguyenphucnha111@gmail.com>
2021-11-04 15:07:01 +0700

    test

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,7 @@ import Routes from "src/routes/routes"
 import ReactGA from "react-ga"
 function App() {
   useEffect(() => {
-    ReactGA.initialize("UA-28382520-33", {
+    ReactGA.initialize("G-7M559PFT09", {
       debug: true
     })
     ReactGA.pageview(window.location.pathname + window.location.search)
