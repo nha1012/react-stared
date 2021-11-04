67c79bc4931e603e3ab5a516d874e470214424df
NHANGUYEN <nguyenphucnha111@gmail.com>
2021-11-04 10:51:38 +0700

    fix

--- src/routes/LoginRoutes.tsx
+++ src/routes/LoginRoutes.tsx
@@ -2,8 +2,8 @@ import React, { lazy, Suspense } from "react"
 import { Route, Switch } from "react-router-dom"
 import { PATH } from "src/constants/paths"
 import Loading from "src/components/Loading/Loading"
-const Login = lazy(() => import("src/pages/Login/Login"))
 import ReactGA from "react-ga"
+const Login = lazy(() => import("src/pages/Login/Login"))
 ReactGA.pageview(window.location.pathname + window.location.search)
 
 export default function LoginRoutes() {
